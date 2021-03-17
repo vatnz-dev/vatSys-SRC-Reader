bb4781ba10ebf27bf6ee049304133261fd12a681
professoralex13 <alex@densem.net>
2021-03-18 10:16:48 +1300

    Added XML Creation Tool

---
+++ vatSys-SRC-Reader/SRCXmlTool/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace SRCXmlTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.AddExtension = false;
+            this.openFileDialog1.DefaultExt = "xml";
+            this.openFileDialog1.FileName = "Routes.xml";
+            this.openFileDialog1.Filter = "XML Files (*.xml)|*.xml|All files (*.*)|*.*";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "XML Chosen: NONE";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(215, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(212, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Airport Code";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 123);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(371, 315);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(429, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(257, 39);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Example:\r\nAACH1 H384 NP H252 NS Y288 (FL240 and above)\r\nAACH6  H384 NP Q137 VELGA" +
+    " Y894\r\n";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(389, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Write to XML";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "XML Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ vatSys-SRC-Reader/SRCXmlTool/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Linq;
+namespace SRCXmlTool
+{
+    public partial class Form1 : Form
+    {
+        string XMLFileLocation;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                XMLFileLocation = openFileDialog1.FileName;
+                label1.Text = "XML File: " + XMLFileLocation;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(XMLFileLocation);
+            var mainNode = doc.DocumentElement;
+            foreach(XmlElement x in mainNode.ChildNodes)
+            {
+                bool breaking = false;
+                foreach(XmlAttribute i in x.Attributes)
+                {
+                    if (i.Name == "Name")
+                    {
+                        if (i.Value == textBox1.Text)
+                        {
+                            richTextBox1.Text += "Found " + textBox1.Text;
+                            mainNode.RemoveChild(x);
+                            breaking = true;
+                            break;
+                        }
+                    }
+                }
+                if (breaking) break;
+            }
+            XmlNode newNode = doc.CreateNode("element", "Airport", "");
+            ((XmlElement)newNode).SetAttribute("Name", textBox1.Text);
+            mainNode.AppendChild(newNode);
+                
+            string[] routeStings = richTextBox1.Text.Split('\n');
+            foreach(string x in routeStings)
+            {                
+                XmlElement newElem = doc.CreateElement("Route");
+                newElem.SetAttribute("ID", x.Substring(0, 5));
+                if (x.Contains('('))
+                {
+                    string remark = x.Split('(')[1];
+                    remark = remark.Substring(0, remark.Length - 1);
+                    newElem.SetAttribute("Remarks", remark);
+
+                    string routing = x.Split('(')[0];
+                    routing = routing.Substring(6, routing.Length - 7);
+                    newElem.InnerText = routing;
+                }
+                else
+                {
+                    newElem.SetAttribute("Remarks", "");
+                    newElem.InnerText = x.Substring(6, x.Length - 6);
+                }
+                newNode.AppendChild(newElem);
+            }
+            doc.Save(XMLFileLocation);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
